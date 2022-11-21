562edbefb8c7e17d031cd2e3a2f42d688537a123
Maor <kubuntu@kubuntu2204.linuxvmimages.local>
2022-11-22 01:05:51 +0200

    main file and the header file
    exit
    ^e
    i
    i main and header filewq
    wq
    :wq
    wq
    main file and header

---
+++ NumClass.h
@@ -0,0 +1,15 @@
+int isArmstrong(int);       
+
+int numOfDigits(int);
+
+int Armstrong(int, int);
+
+int isPalindrome(int);
+
+int isPrime(int);
+
+int isStrong(int);
+
+int factorial(int);
+
+int reverse(int);
---
+++ main.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ 
+ int main(){
+    int a,b;
+    printf("Please enter two numbers to add \n");
+    scanf("%d %d",&a,&b);
+
+    printf("the sum is:%d\n\n", a+b);
+
+
+    return  0;
+ }
